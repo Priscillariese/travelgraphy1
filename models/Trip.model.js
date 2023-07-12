7e1370a4e97cf0aec8d9bc41b56b7dd799c5fe88
Priscilla Riesegit config --global user.email priscilla.riese@gmail.com <priscilla.riese@gmail.com>
2023-07-12 20:05:30 +0200

    sec

---
+++ models/Trip.model.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require("mongoose");
+
+const tripSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    startDate: {
+      type: Date,
+      required: true
+    },
+    endDate: {
+      type: Date,
+      required: true
+    },
+    location: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String // Assuming you store the image URL/path as a string
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Trip = model("Trip", tripSchema);
+
+module.exports = Trip;
