aafe2de9b86ea0e203b60f810d512bb95a1778de
Priscilla Riesegit config --global user.email priscilla.riese@gmail.com <priscilla.riese@gmail.com>
2023-07-19 08:52:13 +0200

    style pages

--- models/user.js
+++ models/user.js
@@ -6,27 +6,6 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
 });
 
-/*userSchema.pre('save', async function (next) {
-  const user = this;
-  if (!user.isModified('password')) return next();
-
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(user.password, salt);
-    user.password = hashedPassword;
-    next();
-  } catch (error) {
-    return next(error);
-  }
-});
-
-userSchema.methods.comparePassword = async function (password) {
-  try {
-    return await bcrypt.compare(password, this.password);
-  } catch (error) {
-    throw new Error(error);
-  }
-};*/
 
 const User = mongoose.model('User', userSchema);
 
